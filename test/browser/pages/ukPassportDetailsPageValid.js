ebef06d7e0b25f7fe366f6178d2658dfb52bff2e
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-04-06 14:24:26 +0100

    Adds unhappy path tests and pages to test folder

---
+++ test/browser/pages/ukPassportDetailsPageValid.js
@@ -0,0 +1,33 @@
+module.exports = class PlaywrightDevPage {
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+    this.url = "http://localhost:5020/ukPassportDetails";
+  }
+
+
+  async isCurrentPage() {
+    return await this.page.url() === this.url;
+  }
+
+  async continue() {
+    await this.page.click("#continue");
+  }
+
+  async expiryDate() {
+    const expDay = new Date().getDate().toString()
+    const currentMonth = new Date().getMonth() + 1
+    const expMonth = currentMonth.toString()
+    const expYear = new Date().getFullYear().toString()
+    await this.page.locator("#ukPassportExpiryDate-day").fill(expDay);
+    await this.page.locator("#ukPassportExpiryDate-month").fill(expMonth);
+    await this.page.locator("#ukPassportExpiryDate-year").fill(expYear);
+  }
+
+  async checkErrorText(){
+    const errorText = await this.page.locator("#error-summary-title").textContent();
+    return errorText.trim(); 
+  }
+};
