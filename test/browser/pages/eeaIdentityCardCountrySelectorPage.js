59659cccbfff79b7eb1c33926a25170d85439438
shenry99 <sadie.henry@digital.cabinet-office.gov.uk>
2023-05-15 15:22:33 +0100

    Added NI EEA Country code automation tests

---
+++ test/browser/pages/eeaIdentityCardCountrySelectorPage.js
@@ -0,0 +1,38 @@
+const { expect } = require('playwright');
+module.exports = class PlaywrightDevPage {
+    /**
+     * @param {import('@playwright/test').Page} page
+     */
+    constructor(page) {
+      this.page = page;
+      this.url = "http://localhost:5030/eeaIdentityCardCountrySelector";
+    }
+    
+    async isCurrentPage() {
+      return await this.page.url() === this.url;
+    }
+  
+    async continue() {
+      await this.page.click("#continue");
+    }
+  
+    async back(){
+      await this.page.click("#back");
+    }
+
+    async checkErrorText(){
+      const errorText = await this.page.locator("#error-summary-title").textContent();
+      return errorText.trim()
+    }
+
+    async selectCountry(){
+      const dropdown = await this.page.locator("select.govuk-select");
+      await dropdown.selectOption("NL");
+    }
+
+    async checkErrorText(){
+      const errorText = await this.page.locator("#error-summary-title").textContent();
+      return errorText.trim(); 
+    }
+
+  };
