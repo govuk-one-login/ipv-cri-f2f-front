805ed1dee76fb3a196a59a17c7e4124525db9ee8
Daniel Page <danielpage@deloitte.co.uk>
2023-08-09 17:46:40 +0100

    base url env var updated

--- run-tests.sh
+++ run-tests.sh
@@ -12,7 +12,7 @@ remove_quotes () {
 export GITHUB_ACTIONS=true
 # shellcheck disable=SC2154
 export IPV_STUB_URL=$(remove_quotes $CFN_F2FIPVStubExecuteURL)start
-export IPV_BASE_URL=$(remove_quotes $CFN_F2FCustomDomain)
+export IPV_BASE_URL=https://$(remove_quotes $CFN_F2FCustomDomain)
 
 cd /app; yarn run test:browser:ci
 
