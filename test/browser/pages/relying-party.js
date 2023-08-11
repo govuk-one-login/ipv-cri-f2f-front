0c497b794b6f3feb0379efd33f717e892c0e1ed8
page-dan <131249716+page-dan@users.noreply.github.com>
2023-08-11 16:27:36 +0100

    Tests for the thin file precess now in place (#133)
    * check for the thin file precess now in place
    
    * emails removed from request
    
    * URL reverted back to build
    
    * un used variable removed
    
    ---------
    
    Co-authored-by: Daniel Page <danielpage@deloitte.co.uk>

--- test/browser/pages/relying-party.js
+++ test/browser/pages/relying-party.js
@@ -4,12 +4,19 @@ module.exports = class PlaywrightDevPage {
    */
   constructor(page) {
     this.page = page;
+    this.axios = require("axios");
+    this.claim = require("../support/shared_claim");
+    this.mockURL = "https://ipvstub.review-o.build.account.gov.uk/start";
   }
-  
-  async goto() {  
-    const axios = require("axios");
-    const claim = require("../support/shared_claim")    
-    const postRequest = await axios.post("https://ipvstub.review-o.build.account.gov.uk/start", claim);    
+
+  async goto() {
+    const postRequest = await this.axios.post(this.mockURL, this.claim);
+    await this.page.goto(postRequest.data.AuthorizeLocation);
+  }
+
+  async goToAsAThinFileUser() {
+    this.claim.evidence_requested.strengthScore = 4;
+    const postRequest = await this.axios.post(this.mockURL, this.claim);
     await this.page.goto(postRequest.data.AuthorizeLocation);
   }
 
---
+++ test/browser/pages/thinFilePhotoIdSelectionPage.js
@@ -0,0 +1,41 @@
+module.exports = class PlaywrightDevPage {
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+    this.path = "/choose-photo-id-post-office-biometric";
+    this.assert = require("assert");
+  }
+
+  async isCurrentPage() {
+    const { pathname } = new URL(await this.page.url());
+    return pathname === this.path;
+  }
+
+  async continue() {
+    await this.page.click("#continue");
+  }
+
+  async ukPassportChoice() {
+    await this.page.click("#photoIdChoiceThinFile");
+  }
+
+  async nonUKPassportChoice() {
+    await this.page.click("#photoIdChoiceThinFile-nonUkPassport");
+  }
+
+  async back() {
+    await this.page.click("#back");
+  }
+
+  async validateThinFileOptions() {
+    this.assert(await this.page.locator("#photoIdChoiceThinFile-ukPassport-label").isVisible(), "UK passport option not visable");
+    this.assert(await this.page.locator("#photoIdChoiceThinFile-nonUkPassport-label").isVisible(), "Non-UK Passport option not visable");
+
+    this.assert(await this.page.locator('#photoIdChoice-euPhotocardDl').count() === 0, "EU photocard displayed for a thin file user");
+    this.assert(await this.page.locator('#eeaIdentityCard').count() === 0, "EEA identity card displayed for a thin file user");
+    this.assert(await this.page.locator('#photoIdChoice-brp').count() === 0, "Biometric residance permet displayed for a thin file user");
+    this.assert(await this.page.locator('#photoIdChoice-ukPhotocardDl').count() === 0, "Uk driving licence displayed for a thin file user");
+  }
+};
