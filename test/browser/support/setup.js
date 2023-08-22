341951322d1f39eff3e4ecd8eca301ea47d63fb5
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2023-08-22 16:21:41 +0100

    increase cucumber timeout

--- test/browser/support/setup.js
+++ test/browser/support/setup.js
@@ -1,6 +1,9 @@
 const { Before, BeforeAll, AfterAll, After } = require("@cucumber/cucumber");
 const { chromium } = require("playwright");
+const { setDefaultTimeout } = require ('@cucumber/cucumber');
 const axios = require("axios");
+
+setDefaultTimeout(10 * 1000);
 
 BeforeAll(async function () {
   // Browsers are expensive in Playwright so only create 1
