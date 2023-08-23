bd690a1ffb350a68e0ce34df776c1d4cfb93ef91
mcgarj <117986969+mcgarj@users.noreply.github.com>
2023-08-23 10:53:36 +0100

    [F2F-1118] - Improve Test Coverage (#145)
    * Adds unit test for landingPageController
    
    * Adds more test cases to photoIdSelection
    
    * Adds unit test for country code files
    
    * Adds unit tests for photoIdExpiry, tries expanding coverage in checkDetails
    
    * Fix shared_claims payload
    
    ---------
    
    Co-authored-by: Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>

--- src/app/f2f/controllers/photoIdSelection.test.js
+++ src/app/f2f/controllers/photoIdSelection.test.js
@@ -28,7 +28,7 @@ describe("PhotoIdSelectionController", () => {
   });
 
   describe("saveValues", () => {
-    it("should save values to sessionModel according to selected document type", async () => {
+    it("should save values to sessionModel according to selected document type - UK Passport", async () => {
 
       req.form.values.photoIdChoice = APP.PHOTO_ID_OPTIONS.UK_PASSPORT;
 
@@ -42,6 +42,91 @@ describe("PhotoIdSelectionController", () => {
         expect(selectedDocument).to.equal("UK passport");
         expect(changeUrl).to.equal("uk-passport-expire");
     });
+
+    it("should save values to sessionModel according to selected document type - BRP", async () => {
+
+      req.form.values.photoIdChoice = APP.PHOTO_ID_OPTIONS.BRP;
+
+        await photoIdSelectionController.saveValues(req, res, next);
+        const selectedDocumentValue = req.sessionModel.get("photoIdChoice")
+        const selectedDocument = req.sessionModel.get("selectedDocument");
+        const changeUrl = req.sessionModel.get("changeUrl");
+
+        expect(next).to.have.been.calledOnce;
+        expect(selectedDocumentValue).to.equal(APP.PHOTO_ID_OPTIONS.BRP)
+        expect(selectedDocument).to.equal("Biometric residence permit (BRP)");
+        expect(changeUrl).to.equal("biometric-residence-permit-expire");
+    });
+
+    it("should save values to sessionModel according to selected document type - UK Driving Licence", async () => {
+
+      req.form.values.photoIdChoice = APP.PHOTO_ID_OPTIONS.UK_PHOTOCARD_DL;
+
+        await photoIdSelectionController.saveValues(req, res, next);
+        const selectedDocumentValue = req.sessionModel.get("photoIdChoice")
+        const selectedDocument = req.sessionModel.get("selectedDocument");
+        const changeUrl = req.sessionModel.get("changeUrl");
+
+        expect(next).to.have.been.calledOnce;
+        expect(selectedDocumentValue).to.equal(APP.PHOTO_ID_OPTIONS.UK_PHOTOCARD_DL)
+        expect(selectedDocument).to.equal("UK photocard driving licence");
+        expect(changeUrl).to.equal("uk-driving-licence-expire");
+    });
+
+    it("should save values to sessionModel according to selected document type - Non-UK Passport", async () => {
+
+      req.form.values.photoIdChoice = APP.PHOTO_ID_OPTIONS.NON_UK_PASSPORT;
+
+        await photoIdSelectionController.saveValues(req, res, next);
+        const selectedDocumentValue = req.sessionModel.get("photoIdChoice")
+        const selectedDocument = req.sessionModel.get("selectedDocument");
+        const changeUrl = req.sessionModel.get("changeUrl");
+
+        expect(next).to.have.been.calledOnce;
+        expect(selectedDocumentValue).to.equal(APP.PHOTO_ID_OPTIONS.NON_UK_PASSPORT)
+        expect(selectedDocument).to.equal("Non-UK passport");
+        expect(changeUrl).to.equal("non-uk-passport-expire");
+    });
+
+    it("should save values to sessionModel according to selected document type - EU Driving Licence", async () => {
+
+      req.form.values.photoIdChoice = APP.PHOTO_ID_OPTIONS.EU_PHOTOCARD_DL;
+
+        await photoIdSelectionController.saveValues(req, res, next);
+        const selectedDocumentValue = req.sessionModel.get("photoIdChoice")
+        const selectedDocument = req.sessionModel.get("selectedDocument");
+        const changeUrl = req.sessionModel.get("changeUrl");
+
+        expect(next).to.have.been.calledOnce;
+        expect(selectedDocumentValue).to.equal(APP.PHOTO_ID_OPTIONS.EU_PHOTOCARD_DL)
+        expect(selectedDocument).to.equal("EU photocard driving licence");
+        expect(changeUrl).to.equal("eu-driving-licence-expire");
+    });
+
+    it("should save values to sessionModel according to selected document type - EEA Identity Card", async () => {
+
+      req.form.values.photoIdChoice = APP.PHOTO_ID_OPTIONS.EEA_IDENTITY_CARD;
+
+        await photoIdSelectionController.saveValues(req, res, next);
+        const selectedDocumentValue = req.sessionModel.get("photoIdChoice")
+        const selectedDocument = req.sessionModel.get("selectedDocument");
+        const changeUrl = req.sessionModel.get("changeUrl");
+
+        expect(next).to.have.been.calledOnce;
+        expect(selectedDocumentValue).to.equal(APP.PHOTO_ID_OPTIONS.EEA_IDENTITY_CARD)
+        expect(selectedDocument).to.equal("National identity card from an EEA country");
+        expect(changeUrl).to.equal("national-identity-card-expire");
+    });
+
+    it("should abort the journey if no photo ID is selected", async () => {
+      req.form.values.photoIdChoice = APP.PHOTO_ID_OPTIONS.NO_PHOTO_ID;
+
+      await photoIdSelectionController.saveValues(req, res, next);
+      const selectedDocumentValue = req.sessionModel.get("photoIdChoice");
+
+      expect(next).to.have.been.calledOnce;
+      expect(selectedDocumentValue).to.equal("noPhotoId");
+    })
   });
 
   describe("saveValues when user selects no option", () => {
@@ -52,7 +137,7 @@ describe("PhotoIdSelectionController", () => {
         await photoIdSelectionController.saveValues(req, res, next);
         const nextError = next.firstArg;
         const nextErrMessage = ("photo-id-selection: Invalid action " + undefined);
-        expect(next).to.have.been.calledOnce;
+        expect(next).to.have.been.calledWith(nextError);
         expect(nextError).to.be.instanceOf(Error);
         expect(nextError.message).to.equal(nextErrMessage);
     });
