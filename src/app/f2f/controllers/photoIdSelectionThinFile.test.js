f361a4627ae0940edf6286ab1e9b3d74bab6cb5d
Caitlin Cooling <40401118+Caitlin-cooling@users.noreply.github.com>
2023-08-30 11:19:01 +0100

    [F2F-1133] increase F2F frontend test coverage (#166)
    * [F2F-1133]  exclude data files from coverage
    
    * [F2F-1133] Adds tests for landing page
    
    * [F2F-1133] updates tests for check details
    
    * [F2F-1133] updates tests for photoIdSelectionThinFile
    
    * [F2F-1133] updates tests for results
    
    * [F2F-1133] Remove logs in case of PII

--- src/app/f2f/controllers/photoIdSelectionThinFile.test.js
+++ src/app/f2f/controllers/photoIdSelectionThinFile.test.js
@@ -27,34 +27,60 @@ describe("PhotoIdSelectionThinFileController", () => {
     expect(photoIdSelectionThinFileController).to.be.an.instanceOf(BaseController);
   });
 
-  describe("saveValues", () => {
-    it("should save values to sessionModel according to selected document type", async () => {
-
+  describe("#saveValues", () => {
+    it("should save values to sessionModel when selected document type is UK_PASSPORT", async () => {
       req.form.values.photoIdChoiceThinFile = APP.PHOTO_ID_OPTIONS.UK_PASSPORT;
 
-        await photoIdSelectionThinFileController.saveValues(req, res, next);
-        const selectedDocumentValue = req.sessionModel.get("photoIdChoice")
-        const selectedDocument = req.sessionModel.get("selectedDocument");
-        const changeUrl = req.sessionModel.get("changeUrl");
+      await photoIdSelectionThinFileController.saveValues(req, res, next);
+      const selectedDocumentValue = req.sessionModel.get("photoIdChoice");
+      const selectedDocument = req.sessionModel.get("selectedDocument");
+      const changeUrl = req.sessionModel.get("changeUrl");
 
-        expect(next).to.have.been.calledOnce;
-        expect(selectedDocumentValue).to.equal(APP.PHOTO_ID_OPTIONS.UK_PASSPORT)
-        expect(selectedDocument).to.equal("UK passport");
-        expect(changeUrl).to.equal("uk-passport-expire");
+      expect(next).to.have.been.calledOnce;
+      expect(selectedDocumentValue).to.equal(APP.PHOTO_ID_OPTIONS.UK_PASSPORT);
+      expect(selectedDocument).to.equal("UK passport");
+      expect(changeUrl).to.equal("uk-passport-expire");
     });
-  });
 
-  describe("saveValues when user selects no option", () => {
-    it("should call next with error", async () => {
+    it("should save values to sessionModel when selected document type is NON_UK_PASSPORT", async () => {
+      req.form.values.photoIdChoiceThinFile = APP.PHOTO_ID_OPTIONS.NON_UK_PASSPORT;
 
+      await photoIdSelectionThinFileController.saveValues(req, res, next);
+      const selectedDocumentValue = req.sessionModel.get("photoIdChoice");
+      const selectedDocument = req.sessionModel.get("selectedDocument");
+      const changeUrl = req.sessionModel.get("changeUrl");
+
+      expect(next).to.have.been.calledOnce;
+      expect(selectedDocumentValue).to.equal(APP.PHOTO_ID_OPTIONS.NON_UK_PASSPORT);
+      expect(selectedDocument).to.equal("Non-UK passport");
+      expect(changeUrl).to.equal("non-uk-passport-expire");
+    });
+
+    it("should save values to sessionModel when selected document type is NO_PHOTO_ID", async () => {
+      req.form.values.photoIdChoiceThinFile = APP.PHOTO_ID_OPTIONS.NO_PHOTO_ID;
+
+      await photoIdSelectionThinFileController.saveValues(req, res, next);
+      const selectedDocumentValue = req.sessionModel.get("photoIdChoice");
+      const selectedDocument = req.sessionModel.get("selectedDocument");
+      const changeUrl = req.sessionModel.get("changeUrl");
+
+      expect(next).to.have.been.calledOnce;
+      expect(selectedDocumentValue).to.equal(APP.PHOTO_ID_OPTIONS.NO_PHOTO_ID);
+      expect(selectedDocument).to.equal(undefined);
+      expect(changeUrl).to.equal(undefined);
+    });
+
+    it("when user selects no option, should call next with invalid action error", async () => {
       req.form.values.photoIdChoice = undefined;
 
-        await photoIdSelectionThinFileController.saveValues(req, res, next);
-        const nextError = next.firstArg;
-        const nextErrMessage = ("photo-id-selection: Invalid action " + undefined);
-        expect(next).to.have.been.calledOnce;
-        expect(nextError).to.be.instanceOf(Error);
-        expect(nextError.message).to.equal(nextErrMessage);
+      await photoIdSelectionThinFileController.saveValues(req, res, next);
+
+      const nextError = next.firstArg;
+      const nextErrMessage = ("photo-id-selection: Invalid action " + undefined);
+
+      expect(next).to.have.been.calledOnce;
+      expect(nextError).to.be.instanceOf(Error);
+      expect(nextError.message).to.equal(nextErrMessage);
     });
   });
-  });
+});
