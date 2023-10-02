1b34cf0b030883413b64c4657a60ec186f3536fb
Viveak Vadivelkarasan <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-10-02 16:27:22 +0100

    KIWI-1285 Improve logging in FE container (#218)

--- src/app/f2f/controllers/landingPage.js
+++ src/app/f2f/controllers/landingPage.js
@@ -1,5 +1,6 @@
 const { Controller: BaseController } = require("hmpo-form-wizard");
 const { API } = require("../../../lib/config");
+const logger = require("hmpo-logger").get();
 
 class LandingPageController extends BaseController {
 
@@ -30,6 +31,7 @@ class LandingPageController extends BaseController {
 			return data;
 		} catch (error) {
 			console.log("Error calling /sessionConfiguration");
+			logger.error("Error calling /sessionConfiguration", error);
 		}
   }
 
