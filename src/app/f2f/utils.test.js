05078a65c795a06f547dbdd287cb6933f5ebe0ea
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2024-02-15 10:32:59 +0000

    Test wording alteration

--- src/app/f2f/utils.test.js
+++ src/app/f2f/utils.test.js
@@ -5,15 +5,15 @@ const moment = require("moment");
 
 describe('formatDate', () => {
   it('returns a YYYY-MM-DD date with language set to en as DD Month(English) YYYY', () => {
-    expect(formatDate('1989-03-31', "en")).to.equal('31 March 1989');
+    expect(formatDate('2030-03-31', "en")).to.equal('31 March 2030');
   })
 
   it('returns a YYYY-MM-DD date with language set to cy as DD Month(Welsh) YYYY', () => {
-    expect(formatDate('1989-03-31', "cy")).to.equal('31 Mawrth 1989');
+    expect(formatDate('2030-03-31', "cy")).to.equal('31 Mawrth 2030');
   })
 
   it('returns a YYYYMM/DD date as Invalid date', () => {
-    expect(formatDate('198903/31', "YYYY-MM-DD")).to.equal('Invalid Date');
+    expect(formatDate('203003/31', "YYYY-MM-DD")).to.equal('Invalid Date');
   })
 
   it('should return an empty string if date is empty string', () => {
