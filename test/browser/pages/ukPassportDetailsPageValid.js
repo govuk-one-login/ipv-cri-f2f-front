07c24a361aea3a956abbfb22ca0b553ebfda9740
Z-Mohamoud <110032361+Z-Mohamoud@users.noreply.github.com>
2024-02-22 16:54:43 +0000

    Update .env logic for F2F (#356)
    * Update .env logic

--- test/browser/pages/ukPassportDetailsPageValid.js
+++ test/browser/pages/ukPassportDetailsPageValid.js
@@ -5,7 +5,7 @@ module.exports = class PlaywrightDevPage {
 
   constructor(page) {
     this.page = page;
-    this.baseURL = process.env.IPV_BASE_URL;
+    this.baseURL = process.env.F2F_FE_BASE_URL;
     this.path = "/uk-passport-expire";
   }
 
