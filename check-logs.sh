b65aacd08bb878d4210095ef9f87a85891acb45c
Bhavana <115095929+hemavana@users.noreply.github.com>
2024-03-13 09:48:57 +0000

    [KIWI-1526] Adding PII check-logs script for automated tests (#379)
    * Adding PII check-logs script for automated tests
    
    * updated the query logic
    
    * Updating the shared_claims value to contain unique values.

---
+++ check-logs.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+test_data="./test/browser/support/shared_claim.json"
+firstName=$(jq -r '.shared_claims.name[0].nameParts[0].value' "$test_data")
+firstName1=$(jq -r '.shared_claims.name[0].nameParts[1].value' "$test_data")
+lastName=$(jq -r '.shared_claims.name[0].nameParts[2].value' "$test_data")
+birthDate=$(jq -r '.shared_claims.birthDate[0].value' "$test_data")
+emailAddress=$(jq -r '.shared_claims.emailAddress' "$test_data")
+address_postalCode=$(jq -r '.shared_claims.address[0].postalCode' "$test_data")
+
+query="fields @timestamp, @message, @logStream, @log | filter @message like \"$firstName\""
+
+function update_query_string() {
+  # Get the array of search strings as arguments  
+  local searchStrings=("$@")
+
+  for value in "${searchStrings[@]}"
+  do
+    query+=" or @message like \"$value\""
+  done
+
+  # Return the updated query string
+  echo $query
+}
+
+query=$(update_query_string $firstName1 $lastName $birthDate $emailAddress $address_postalCode)
+echo $query
+
+stack_name="f2f-cri-front"
+log_groups=(
+    "/aws/ecs/$stack_name-F2FFront-ECS"
+)
+
+current_epoch=$(date +%s)
+fifteen_mins_ago_epoch=$((current_epoch - (15 * 60)))
+
+start_time=$fifteen_mins_ago_epoch
+end_time=$current_epoch
+
+query_id=$(aws logs start-query \
+    --log-group-names "${log_groups[@]}" \
+    --start-time "$start_time" \
+    --end-time "$end_time" \
+    --query-string "$query" \
+    --output text --query 'queryId')
+
+status="Running"
+while [ "$status" = "Running" ]; do
+    echo "Waiting for query to complete..."
+    sleep 1
+    query_status=$(aws logs get-query-results --query-id "$query_id")
+    status=$(echo "$query_status" | grep -o '"status": "[^"]*"' | cut -d '"' -f 4)
+done
+
+if echo "$query_status" | grep -q '"results": \[\]'; then
+    echo "Query found no PII 🎉"
+    exit 0
+else
+    echo "Query returned results:"
+    echo "$query_status" | jq -r '.results[] | @json'
+    exit 1
+fi
