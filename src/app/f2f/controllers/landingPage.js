38a074e7a2018e3c97af551c3c0483cd6b40f934
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2024-07-23 09:10:53 +0100

    pclEnabled sess model property set to false by default

--- src/app/f2f/controllers/landingPage.js
+++ src/app/f2f/controllers/landingPage.js
@@ -5,6 +5,7 @@ const logger = require("hmpo-logger").get();
 class LandingPageController extends BaseController {
   async saveValues(req, res, next) {
     req.sessionModel.set("isThinFileUser", false);
+    req.sessionModel.set("pclEnabled", false);
 
     try {
       const configData = await this.getSessionConfig(req, res);
