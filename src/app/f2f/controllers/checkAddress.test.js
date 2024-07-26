8a45d869fa26ab1bfabad2b5525ca004d72bdd97
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2024-07-26 13:12:48 +0100

    Remove locals test

--- src/app/f2f/controllers/checkAddress.test.js
+++ src/app/f2f/controllers/checkAddress.test.js
@@ -3,42 +3,9 @@ const { expect } = require("chai");
 const CheckAddressController = require("./checkAddress");
 
 describe("CheckAddress controller", () => {
-    let checkAddressController;
-    let req;
-    let res;
-    let next;
+  const checkAddressController = new CheckAddressController({ route: "/test" });
   
-    beforeEach(() => {
-      const setup = setupDefaultMocks();
-      req = setup.req;
-      res = setup.res;
-      next = setup.next;
-  
-      checkAddressController = new CheckAddressController({ route: "/test" });
-    });
-  
-    it("should be an instance of BaseController", () => {
-      expect(checkAddressController).to.be.an.instanceOf(BaseController);
-    });
-
-    it("should set locals to shared claims address values", () => {
-        const testAddressLine1 = "testAddressLine1"
-        const testAddressLine2 = "testAddressLine2"
-        const testTownCity = "testTownCity"
-        const testPostalCode = "testPostalCode"
-        req.sessionModel.set("addressLine1", testAddressLine1)
-        req.sessionModel.set("addressLine2", testAddressLine2)
-        req.sessionModel.set("townCity", testTownCity)
-        req.sessionModel.set("postalCode", testPostalCode)
-
-        checkAddressController.locals(req, res, next)
-
-    setImmediate(() => {
-      const locals = next.args[0][1];
-      expect(locals.addressLine1).to.equal(testAddressLine1);
-      expect(locals.addressLine2).to.equal(testAddressLine2);
-      expect(locals.townCity).to.equal(testTownCity);
-      expect(locals.postalCode).to.equal(testPostalCode);
-    });
-      });
-})
+  it("should be an instance of BaseController", () => {
+    expect(checkAddressController).to.be.an.instanceOf(BaseController);
+  });
+});
