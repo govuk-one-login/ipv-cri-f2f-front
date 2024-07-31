e9fd6f21f82f87a3b65f5eee2dcafecb4a23e66d
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2024-07-31 10:16:44 +0100

    FE fix

--- src/app/f2f/controllers/checkDetails.js
+++ src/app/f2f/controllers/checkDetails.js
@@ -95,7 +95,7 @@ class CheckDetailsController extends DateController {
       let idHasExpiryDate;
       let expiryDate;
       let address;
-      const pdfPreference = req.form.values.postOfficeCustomerLetterChoice
+      //const pdfPreference = req.form.values.postOfficeCustomerLetterChoice;
 
       switch (req.form.values.photoIdChoice) {
         case APP.PHOTO_ID_OPTIONS.UK_PASSPORT: {
@@ -166,7 +166,7 @@ class CheckDetailsController extends DateController {
       req.sessionModel.set("idHasExpiryDate", idHasExpiryDate);
       req.sessionModel.set("expiryDate", expiryDate);
       req.sessionModel.set("addressCheck", address);
-      req.sessionModel.set("pdfPreference", pdfPreference);
+      //req.sessionModel.set("pdfPreference", pdfPreference);
 
       //Confirmation display values
       const idChoice = req.sessionModel.get("photoIdChoice");
@@ -216,7 +216,7 @@ class CheckDetailsController extends DateController {
           post_code: req.sessionModel.get("postOfficePostcode"),
           fad_code: req.sessionModel.get("postOfficeFadCode"),
         },
-        pdf_preference: req.sessionModel.get("pdfPreference"),
+        pdf_preference: "EMAIL_ONLY",
       };
 
       await this.saveF2fData(req.axios, f2fData, req, res);
