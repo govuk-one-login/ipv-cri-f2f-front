829ebbe3d2b9d641989e0ac836db7ab194c31716
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2024-08-13 17:22:53 +0100

    Root controller commented out axios call throwing errors

--- src/app/f2f/controllers/root.js
+++ src/app/f2f/controllers/root.js
@@ -1,7 +1,7 @@
 const { Controller: BaseController } = require("hmpo-form-wizard");
 const { API } = require("../../../lib/config");
-const logger = require("hmpo-logger").get();
-const NodeRSA = require("node-rsa");
+// const logger = require("hmpo-logger").get();
+// const NodeRSA = require("node-rsa");
 const {
   createPersonalDataHeaders,
 } = require("@govuk-one-login/frontend-passthrough-headers");
@@ -11,22 +11,22 @@ class RootController extends BaseController {
 
     const sharedClaims = req.session?.shared_claims;
 
-    try { 
-      const encryptedJSON = await this.getAddressInfo(req.axios, req);
-      const key = await this.getDecryptKey(req.axios, req);
-      const decryptKey = new NodeRSA(key)
-      const userAddress = decryptKey.decrypt(encryptedJSON, "utf8")
-      const parsedAddress = JSON.parse(userAddress)
+    // try { 
+    //   const encryptedJSON = await this.getAddressInfo(req.axios, req);
+    //   const key = await this.getDecryptKey(req.axios, req);
+    //   const decryptKey = new NodeRSA(key)
+    //   const userAddress = decryptKey.decrypt(encryptedJSON, "utf8")
+    //   const parsedAddress = JSON.parse(userAddress)
 
-      req.sessionModel.set("addressLine1", parsedAddress["address_line1"])
-      req.sessionModel.set("addressLine2", parsedAddress["address_line2"])
-      req.sessionModel.set("townCity", parsedAddress["town_city"])
-      req.sessionModel.set("postalCode", parsedAddress["postal_code"])
+    //   req.sessionModel.set("addressLine1", parsedAddress["address_line1"])
+    //   req.sessionModel.set("addressLine2", parsedAddress["address_line2"])
+    //   req.sessionModel.set("townCity", parsedAddress["town_city"])
+    //   req.sessionModel.set("postalCode", parsedAddress["postal_code"])
 
-    } catch(error) {
-      console.log("Error calling /person-info")
-      logger.error("Error calling /person-info", error);
-    }
+    // } catch(error) {
+    //   console.log("Error calling /person-info")
+    //   logger.error("Error calling /person-info", error);
+    // }
 
     if (sharedClaims) {
       if (sharedClaims?.address[0]?.postalCode?.length > 0) {
