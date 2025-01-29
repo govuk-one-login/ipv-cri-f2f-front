cbed4ac853eff1731b7bcd014b270fd5204b3346
ZacMohamoud01 <zac.mohamoud@digital.cabinet-office.gov.uk>
2025-01-29 12:31:12 +0000

    update PCL logic

---
+++ test/browser/step_definitions/F2FPostOfficeCustomerLetter.js
@@ -0,0 +1,21 @@
+const { Given, Then } = require("@cucumber/cucumber");
+
+const { PostOfficeCustomerLetter } = require("../pages");
+const { expect } = require("chai");
+
+Given(
+  /^the user navigates directly to post-office-customer-letter$/,
+  async function () {
+    const postOfficeCustomerLetterPage = new PostOfficeCustomerLetter(this.page);
+    await postOfficeCustomerLetterPage.goTo();
+  }
+);
+
+When(
+  /^the user selects an Email only Post Office Letter$/,
+  async function () {
+    const postOfficeCustomerLetterPage = new PostOfficeCustomerLetter(this.page);
+    await postOfficeCustomerLetterPage.emailOnly();
+    await postOfficeCustomerLetterPage.continue();
+  }
+);
