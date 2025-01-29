654b85f999bfe8581dd1754abb79bb68c5a64efa
ZacMohamoud01 <zac.mohamoud@digital.cabinet-office.gov.uk>
2025-01-29 13:00:39 +0000

    Update validation for failing test

--- test/browser/step_definitions/F2FPostOfficeCustomerLetter.js
+++ test/browser/step_definitions/F2FPostOfficeCustomerLetter.js
@@ -2,6 +2,8 @@ const { Given, When, Then } = require("@cucumber/cucumber");
 
 const { PostOfficeCustomerLetter } = require("../pages");
 
+const { expect } = require("chai");
+
 Given(
   /^the user navigates directly to post-office-customer-letter$/,
   async function () {
