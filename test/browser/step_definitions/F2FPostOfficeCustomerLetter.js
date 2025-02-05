eb47a1ca46f7b8cb9a05f9bbfac8d656d84a374c
“Zac <zac.mohamoud@digital.cabinet-office.gov.uk>
2025-02-05 11:07:57 +0000

    updates to PCL tests + Posted Letter logic

--- test/browser/step_definitions/F2FPostOfficeCustomerLetter.js
+++ test/browser/step_definitions/F2FPostOfficeCustomerLetter.js
@@ -20,6 +20,12 @@ When(/^the user selects an Email only Post Office Letter$/, async function () {
   await postOfficeCustomerLetterPage.continue();
 });
 
+When(/^the user selects an Email and Post Office Letter$/, async function () {
+  const postOfficeCustomerLetterPage = new PostOfficeCustomerLetter(this.page);
+  await postOfficeCustomerLetterPage.emailAndPost();
+  await postOfficeCustomerLetterPage.continue();
+});
+
 Then(
   /^the user is navigated back to the PO Customer Letter page$/,
   async function () {
