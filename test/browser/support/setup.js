89967a2d4b48a60679d87ccead77c5c6b2099695
“Zac <zac.mohamoud@digital.cabinet-office.gov.uk>
2025-02-06 11:12:34 +0000

    PlayWright setup revert changes

--- test/browser/support/setup.js
+++ test/browser/support/setup.js
@@ -11,9 +11,9 @@ BeforeAll(async function () {
     ? await chromium.launch()
     : await chromium.launch({
         // Not headless so we can watch test runs
-        headless: true,
+        headless: false,
         // Slow so we can see things happening
-        // slowMo: 1500,
+        //slowMo: 1500,
       });
 });
 
