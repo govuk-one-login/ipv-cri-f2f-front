24f34baf006dab0fffeffe39b07d6888d2c02189
MakiniKayiraGDS <makini.kayira@digital.cabinet-office.gov.uk>
2025-02-12 15:02:55 +0000

    [KIWI-535] Add missing tests and remove checkDetails.js invalid test

--- src/app/f2f/controllers/eeaIdentityCardDetails.test.js
+++ src/app/f2f/controllers/eeaIdentityCardDetails.test.js
@@ -10,4 +10,13 @@ describe("EuIdentityCardController", () => {
   it("should be an instance of BaseController", () => {
     expect(eeaIdentityCardController).to.be.an.instanceOf(BaseController);
   });
+
+  it("should not pad the year field", async () => {
+    var value = "30";
+    var offset = 0;
+
+    var finalYear = await eeaIdentityCardController._padYear(value, offset);
+
+    expect(finalYear).to.equal("30");
+  });
 });
