0536d18cacfba492ad8f0b0d6d46239b4b899067
Mkayira <makini.kayira@digital.cabinet-office.gov.uk>
2025-02-18 10:31:24 +0000

    [KIWI-535] Only Accept 4-digit Entries for Year Field (#549)
    * [KIWI-535] Only Accept 4-digit Entries for Year Field
    
    * [KIWI-535] Add validations for each individual ID page
    
    * [KIWI-535] Add missing tests and remove checkDetails.js invalid test

--- src/app/f2f/controllers/euPhotocardDlDetails.js
+++ src/app/f2f/controllers/euPhotocardDlDetails.js
@@ -2,7 +2,13 @@ const BaseController = require("hmpo-form-wizard").Controller;
 const DateControllerMixin = require("hmpo-components").mixins.Date;
 
 const DateController = DateControllerMixin(BaseController);
+const logger = require("hmpo-logger").get();
 
-class EuPhotocardDlController extends DateController {}
+class EuPhotocardDlController extends DateController {
+  _padYear(value, offset) {
+    logger.info("offset value of {} ignored as no padding is applied", offset);
+    return value;
+  }
+}
 
 module.exports = EuPhotocardDlController;
--- src/app/f2f/controllers/ukPassportDetails.test.js
+++ src/app/f2f/controllers/ukPassportDetails.test.js
@@ -10,4 +10,13 @@ describe("PassportDetailsController", () => {
   it("should be an instance of BaseController", () => {
     expect(passportDetailsController).to.be.an.instanceOf(BaseController);
   });
+
+  it("should not pad the year field", async () => {
+    var value = "30";
+    var offset = 0;
+
+    var finalYear = await passportDetailsController._padYear(value, offset);
+
+    expect(finalYear).to.equal("30");
+  });
 });
