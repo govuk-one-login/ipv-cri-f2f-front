24f0897ef7ed8a4b0ab8ef2711a05ce0d52b46dc
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2025-11-03 15:30:42 +0000

    Revert "Test env vars in script file"
    This reverts commit 4cf9ac395ff7e84b1680a83f53977a97823b0a3b.

--- run-tests.sh
+++ run-tests.sh
@@ -11,20 +11,12 @@ remove_quotes () {
 # Github actions set to true for tests to run in headless mode
 export GITHUB_ACTIONS=true
 # shellcheck disable=SC2154
-echo "--- Setting Environment Variables ---"
-export IPV_STUB_URL=$(remove_quotes "$CFN_F2FIPVStubExecuteURL")
-echo "IPV_STUB_URL=${IPV_STUB_URL}"
-export F2F_FE_BASE_URL=https://$(remove_quotes "$CFN_F2FCustomDomain")
-echo "F2F_FE_BASE_URL=${F2F_FE_BASE_URL}"
-export TEST_HARNESS_URL=$(remove_quotes "$CFN_F2FTestHarnessURL")
-echo "TEST_HARNESS_URL=${TEST_HARNESS_URL}"
-export SESSION_TABLE=$(remove_quotes "$CFN_F2FBackendSessionTableName")
-echo "SESSION_TABLE=${SESSION_TABLE}"
+export IPV_STUB_URL=$(remove_quotes $CFN_F2FIPVStubExecuteURL)
+export F2F_FE_BASE_URL=https://$(remove_quotes $CFN_F2FCustomDomain)
+export TEST_HARNESS_URL=$(remove_quotes $CFN_F2FTestHarnessURL)
+export SESSION_TABLE=$(remove_quotes $CFN_F2FBackendSessionTableName)
 export API_BASE_URL=$(remove_quotes "$CFN_F2FBackendURL")
-echo "API_BASE_URL=${API_BASE_URL}"
 export LANGUAGE_TOGGLE_DISABLED=false
-echo "LANGUAGE_TOGGLE_DISABLED=${LANGUAGE_TOGGLE_DISABLED}"
-echo "--- Environment Variables Set ---"
 
 declare error_code
 
