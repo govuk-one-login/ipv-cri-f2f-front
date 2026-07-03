618337160dcda3fd206a5fcdade6856345295403
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2026-07-03 16:31:51 +0100

    Test added for locals coverage

--- src/app/f2f/controllers/postOfficeCustomerLetter.test.js
+++ src/app/f2f/controllers/postOfficeCustomerLetter.test.js
@@ -1,5 +1,6 @@
 const BaseController = require("hmpo-form-wizard").Controller;
 const { expect } = require("chai");
+const { APP } = require("../../../lib/config");
 const PostOfficeCustomerLetterController = require("./postOfficeCustomerLetter");
 
 describe("CheckAddress controller", () => {
@@ -7,5 +8,13 @@ describe("CheckAddress controller", () => {
 
   it("should be an instance of BaseController", () => {
     expect(postOfficeCustomerLetterController).to.be.an.instanceOf(BaseController);
+  });
+
+  it("saves the APP.LETTER_LANGUAGE_CHOICE_ENABLED value to locals", () => {
+    const req = {};
+    const res = {};
+    postOfficeCustomerLetterController.locals(req, res, (err, locals) => {
+      expect(locals.letterLanguageChoiceEnabled).to.equal(APP.LETTER_LANGUAGE_CHOICE_ENABLED);
+    });
   });
 });
